2181a52442971759a4a20e299d5f8f5400959d19
Apaark13 <apaark13@gmail.com>
2023-12-25 23:11:45 +0530

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-2lo6u9$tm!q_ulxvtpg#r^@p8*i4m6e+f-6&a%&x#kf)ru4r3h"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
